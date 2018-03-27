c59a85fc03403fc5d43379136ab28828d2b41482
Alexander Thomas <s179069@students.aie.edu.au>
2018-03-27 12:33:06 -0700

    Add win condition & score UI

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -5,8 +5,11 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour {
 
+    public int pointsToWin = 1;
+
     public Text scoreText1;
     public Text scoreText2;
+    public Text winText;
 
     private int score1 = 0;
     private int score2 = 0;
@@ -23,12 +26,24 @@ public class ScoreManager : MonoBehaviour {
     {
         score1 += amt;
         scoreText1.text = score1.ToString();
+
+        if(score1 >= pointsToWin)
+        {
+            Time.timeScale = 0;
+            winText.text = "PLAYER 1 WINS!";
+        }
     }
 
     public void AddScoreP2(int amt)
     {
         score2 += amt;
         scoreText2.text = score2.ToString();
+
+        if (score2 >= pointsToWin)
+        {
+            Time.timeScale = 0;
+            winText.text = "PLAYER 2 WINS!";
+        }
     }
 
 
